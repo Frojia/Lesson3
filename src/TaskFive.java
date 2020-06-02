781f1409dbe058ce460d8b8b4cb6cf297e52fa24
Frojia <65237608+Frojia@users.noreply.github.com>
2020-06-02 15:25:47 +0300

    Update TaskFive.java

--- src/TaskFive.java
+++ src/TaskFive.java
@@ -2,7 +2,7 @@ public class TaskFive {
     public static void main (String[] args) {
         String a = "ABCDEFGHIJK";
         a = a.toLowerCase();
-        a = a.replaceAll("[AEIOUaeiou]","");
+        a = a.replaceAll("[aeiou]","");
         System.out.println(a);
     }
 }
