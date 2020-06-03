c9e1ce9f67e5ce4f915a1f5e853fd2cd5d394641
Frojia <65237608+Frojia@users.noreply.github.com>
2020-06-03 11:56:15 +0300

    Update TaskEight.java

--- src/TaskEight.java
+++ src/TaskEight.java
@@ -1,6 +1,16 @@
 public class TaskEight {
-    public static void main (String[] args) {
-        String b = String.join("\n", "Den", "John", "Will", "Kate", "Adam", "Robin");
-        System.out.println(b);
+    public static void Names(String a) {
+        String[] result;
+        result = a.split(", ");
+        for (String b : result) {
+            System.out.println(b);
+        }
     }
+
+    public static void main(String[] args) {
+        System.out.println("Sort Names");
+        String a = "Den, John, Will, Kate, Adam, Robin";
+        Names(a);
+    }
+
 }
